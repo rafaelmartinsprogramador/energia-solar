2b99b87276426d4e5b7bc02214f0a8d6516071b2
Rafael da Silva Martins <124901505+rafaelmartinsprogramador@users.noreply.github.com>
2023-08-19 13:12:59 -0300

    comit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+window.sr = ScrollReveal ({ reset: true});
+
+sr.reveal('#sobreenergia1', { rotate: { x:80, y: 0, z: 0}, duration: 2000 });
+sr.reveal('#sobreenergia2', { rotate: { x:80, y: 0, z: 0}, duration: 2100 });
+sr.reveal('#sobreenergia3', { rotate: { x:80, y: 0, z: 0}, duration: 2200 });
+
+sr.reveal('.escolha', { rotate: { x:80, y: 0, z: 0}, duration: 2000 });
+
+sr.reveal('#grafico1', { rotate: { x:80, y: 0, z: 0}, duration: 2000 });
+sr.reveal('#grafico2', { rotate: { x:80, y: 0, z: 0}, duration: 2200 });
+sr.reveal('#grafico3', { rotate: { x:80, y: 0, z: 0}, duration: 2400 });
+
+sr.reveal('.bannerempresa', { rotate: { x:80, y: 0, z: 0}, duration: 2000 });
+
+sr.reveal('.nossosservicos', { rotate: { x:80, y: 0, z: 0}, duration: 2000 });
+
+sr.reveal('#atendimentos1', { rotate: { x:80, y: 0, z: 0}, duration: 2000 });
+sr.reveal('#atendimentos2', { rotate: { x:80, y: 0, z: 0}, duration: 2100 });
+sr.reveal('#atendimentos3', { rotate: { x:80, y: 0, z: 0}, duration: 2200 });
